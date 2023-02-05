4d60775c15caefd28bf0395210e11df8eeac29d4
Balpreet-Dhillon <109980525+Balpreet-Dhillon@users.noreply.github.com>
2023-02-05 15:59:25 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+//Using Async Await
+async function generateJoke(){
+
+    const config = {
+
+        headers: {
+
+            Accept: 'application/json',
+        },
+    }
+
+     const response = await fetch('https://icanhazdadjoke.com/', config)
+    
+     const data = await response.json()
+
+     jokeElement.innerHTML = data.joke;
+
+}
+
+//Using .then()
+// function generateJoke(){
+
+//     const config = {
+
+//         headers: {
+
+//             Accept: 'application/json',
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', config)
+    
+//     .then((response) => response.json())
+//     .then((data) => {
+
+//         jokeElement.innerHTML = data.joke;
+
+
+//     })
+// }
